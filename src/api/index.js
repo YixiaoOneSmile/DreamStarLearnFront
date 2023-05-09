292c06cb3ef752aac09d27a5bbfb81e2f5f53a9e
tongque <tongque00@gmail.com>
2023-05-09 20:38:19 +0800

    improve the course page

--- src/api/index.js
+++ src/api/index.js
@@ -27,41 +27,49 @@ export const register = async (id, password) => {
 //获取课程
 export const getCourses = async () => {
   try {
-    const response = await axios.get('/api/courses');
+    const response = await axios.get('http://localhost:3000/getAllCourses');
     return response;
   } catch (error) {
     throw new Error(error);
   }
 };
 
-export const getCourse= async (courseId) => {
-    try {
-      const response = await axios.get('/api/courses');
-      return response;
-    } catch (error) {
-      throw new Error(error);
-    }
-  };
+export const getCourse = async (courseId) => {
+  try {
+    const response = await axios.get(`http://localhost:3000/getAllCourses/${courseId}`);
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
-  export const createCourse =async()=>{
 
-  }
-  export const createExam =async()=>{
-    
-  }
-  export const getExam =async()=>{
-    
-  }
-  export const getExams =async()=>{
-    
-  }
-  export const registerUser =async()=>{
-    
-  }
-  export const submitExam =async()=>{
-    
-  }
- 
-  export const getExamResult =async()=>{
-    
-  }
+export const createCourse = async (course) => {
+  const response = await axios.post('http://localhost:3000/createCourse', course);
+  return response.data;
+}
+
+export const createChapter = async (courseId, chapterData) => {
+  const response = await axios.post(`http://localhost:3000/courses/${courseId}`, chapterData);
+  return response;
+};
+
+export const createExam = async () => {
+
+}
+export const getExam = async () => {
+
+}
+export const getExams = async () => {
+
+}
+export const registerUser = async () => {
+
+}
+export const submitExam = async () => {
+
+}
+
+export const getExamResult = async () => {
+
+}
--- src/pages/CourseListPage/CourseListPage.jsx
+++ src/pages/CourseListPage/CourseListPage.jsx
@@ -1,70 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // 引入 Link 组件
+import { Link } from 'react-router-dom';
 import { getCourses } from '../../api/index.js';
-import { List, Card, Button } from 'antd';
+import { List, Card, Button, Typography } from 'antd';
 
-const mockCourses = [
-  {
-    _id: '1',
-    title: 'Course 1',
-    description: 'This is a description for Course 1.',
-  },
-  {
-    _id: '2',
-    title: 'Course 2',
-    description: 'This is a description for Course 2.',
-  },
-  {
-    _id: '3',
-    title: 'Course 3',
-    description: 'This is a description for Course 3.',
-  },
-  {
-    _id: '4',
-    title: 'Course 4',
-    description: 'This is a description for Course 4.',
-  },
-  {
-    _id: '4',
-    title: 'Course 4',
-    description: 'This is a description for Course 4.',
-  },
-  {
-    _id: '4',
-    title: 'Course 4',
-    description: 'This is a description for Course 4.',
-  },
-];
+const { Title } = Typography;
 
 const CourseListPage = () => {
-  const [courses, setCourses] = useState(mockCourses);
+  const [courses, setCourses] = useState([]);
+
   useEffect(() => {
     const fetchCourses = async () => {
       try {
         const response = await getCourses();
-        setCourses(response.data);
+        setCourses(response.data.data);
       } catch (error) {
         // 显示错误信息
       }
     };
-
     fetchCourses();
   }, []);
 
   return (
-    <div>
-      <h1>Courses</h1>
+    <div style={{ padding: '24px' }}>
+      <Title level={2}>启梦课程</Title>
       <List
         grid={{ gutter: 16, column: 4 }}
         dataSource={courses}
         renderItem={(course) => (
           <List.Item>
             <Card
-              title={course.title}
+              hoverable
+              style={{ width: '100%' }}
+              title={course.name}
               extra={
-                // 使用 Link 组件替换 Button 组件
                 <Link to={`/courses/${course._id}`}>
-                  <Button type="primary">View</Button>
+                  <Button type="primary">详情</Button>
                 </Link>
               }
             >
